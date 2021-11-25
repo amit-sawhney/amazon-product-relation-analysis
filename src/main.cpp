aa96fbdf8f30f99abc13706d6905b261e533bdfe
Amit Sawhney <sawhney4@siebl-0224-08.ews.illinois.edu>
2021-11-24 18:07:00 -0600

    Add basic project structure

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "I compiled" << endl;
+
+    return 0;
+}
---
+++ tests/tests.cpp
@@ -0,0 +1,6 @@
+#define CATCH_CONFIG_MAIN
+#include "../catch/catch.hpp"
+
+TEST_CASE("The Makefile executes properly", "[compilation-check") {
+  REQUIRE(true);
+}
