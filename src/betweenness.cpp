9dfacd94484118b89b74ef6e0f400c97640eaea3
Daniel Moon <dm32@linux-a3.ews.illinois.edu>
2021-12-11 16:04:46 -0600

    finished accumulateNode helper

--- src/betweenness.cpp
+++ src/betweenness.cpp
@@ -1,19 +1,34 @@
 #include "betweenness.h"
 
-Betweenness::Betweenness() {}
+// Betweenness::Betweenness() {}
 
-Betweenness::Betweenness(vector<Node>* nodes, AdjList* adjacency) {
-
+Betweenness::Betweenness(const vector<Node>& nodes, const AdjList& adjacency) {
+    nodes_ = nodes;
+    adjacency_ = adjacency;
 }
 
-void calculateBetweenness() {
-
+map<Node*, double> calculateBetweenness() {
+    return betweenness_values_;
 }
 
 void accumulateNodes(Node* node) {
+    map<Node*, double> delta = {};
 
+    while (!node_stack_.empty()) {
+        Node* next_node = node_stack_.top();
+        node_stack_.pop();
+
+        double coeff = (1 + delta[next_node]) / sigma[next_node];
+        for(auto vertex: predecessors_) {
+            delta[next_node] = delta[next_node] + sigma_[vertex] * coeff;
+        }
+
+        if (next_node != node) {
+            betweenness_values_[next_node] = betweenness_values_[next_node] + delta[next_node];
+        }
+    }
 }
 
 void shortestPathCaculation(Node* node) {
-    
+
 }
